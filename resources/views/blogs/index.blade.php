703165ad79dbbbd012cad03ec1d1dd92f1890d5d
samithseu <samithseu2002@gmail.com>
2025-02-23 00:27:14 +0700

    used `route()` with parameter

--- resources/views/blogs/index.blade.php
+++ resources/views/blogs/index.blade.php
@@ -6,7 +6,8 @@
         @foreach ($blogs as $blog)
             <div
                 class="flex flex-col gap-2 border rounded-xl border-green-500 p-4 bg-gradient-to-br hover:from-green-500/40 hover:to-transparent transition-all duration-300">
-                <a href="/blogs/{{ $blog->id }}" class="text-xl font-bold line-clamp-1 mb-2">{{ $blog->title }}</a>
+                <a href="{{ route('blog.show', $blog->id) }}"
+                    class="text-xl font-bold line-clamp-1 mb-2">{{ $blog->title }}</a>
                 <p class="text-sm text-gray-500 line-clamp-2">{{ $blog->body }}</p>
             </div>
         @endforeach
