63954b7efcfd30010bf32f5e66d0cd2eaefa7d87
samithseu <samithseu2002@gmail.com>
2025-02-23 00:21:23 +0700

    added `about` & `projects` pages

---
+++ resources/views/projects.blade.php
@@ -0,0 +1,13 @@
+@section('title', 'Projects')
+
+<x-layout>
+    <x-section-title>Projects</x-section-title>
+    <p class="text-justify text-gray-400 leading-normal">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt
+        nobis molestias molestiae sequi iure earum velit!
+        Dolorem repudiandae animi nisi, dolorum ullam, cupiditate sequi error deserunt at inventore atque libero
+        exercitationem architecto blanditiis possimus commodi? Quas fuga illo odio exercitationem. Harum ratione eaque
+        minus reiciendis obcaecati veritatis, in maxime suscipit facilis temporibus pariatur odio? Vel, ut aspernatur,
+        vitae nulla suscipit nemo inventore harum alias similique aliquid facilis quis error quae dolore beatae
+        consequuntur ea eius a, omnis nobis totam explicabo. Eveniet iure qui libero, hic nobis suscipit aspernatur
+        rerum cupiditate asperiores, minus, necessitatibus neque ab error in officiis nam harum.</p>
+</x-layout>
