26d8ee47a3b5473c00d3cebd942589f993e9a1c6
samithseu <samithseu2002@gmail.com>
2025-02-23 00:21:54 +0700

    added views for new pages

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,14 @@ Route::get('/', function () {
     return view('welcome');
 })->name("home");
 
+Route::get('/about', function () {
+    return view('about');
+})->name("about");
+
+Route::get('/projects', function () {
+    return view('projects');
+})->name("projects");
+
 Route::get('/blogs', [BlogController::class, "index"])->name("blogs");
 Route::get('/blogs/create', [BlogController::class, "create"])->name("blog.create");
 Route::get('/blogs/{id}', [BlogController::class, "show"])->name("blog.show");
