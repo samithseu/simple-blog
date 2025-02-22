ba9c6bd1a36fa88bf9790b0bedd9d13b1eb9b3c7
samithseu <samithseu2002@gmail.com>
2025-02-22 22:33:04 +0700

    added `name` for routes

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name("home");
 
-Route::get('/home', function () {
-    return view('home');
-});
+Route::get('/blogs', function () {
+    // make API request
+    $response = file_get_contents('https://jsonplaceholder.typicode.com/posts');
+    $posts = json_decode($response);
+
+    return view('blogs.index', compact('posts'));
+})->name("blogs");
+
