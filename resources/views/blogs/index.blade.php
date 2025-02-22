61ce26ba37cb195ea399b10d5b1c601d83f70da8
samithseu <samithseu2002@gmail.com>
2025-02-23 00:00:48 +0700

    fetched all blogs from DB and added URL to each blog

--- resources/views/blogs/index.blade.php
+++ resources/views/blogs/index.blade.php
@@ -3,11 +3,11 @@
 <x-layout>
     <x-section-title>Blogs</x-section-title>
     <div class="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-        @foreach ($posts as $post)
+        @foreach ($blogs as $blog)
             <div
                 class="flex flex-col gap-2 border rounded-xl border-green-500 p-4 bg-gradient-to-br hover:from-green-500/40 hover:to-transparent transition-all duration-300">
-                <a href="/" class="text-xl font-bold line-clamp-2">{{ $post->title }}</a>
-                <p class="text-sm text-gray-400 line-clamp-2">{{ $post->body }}</p>
+                <a href="/blogs/{{ $blog->id }}" class="text-xl font-bold line-clamp-1 mb-2">{{ $blog->title }}</a>
+                <p class="text-sm text-gray-500 line-clamp-2">{{ $blog->body }}</p>
             </div>
         @endforeach
     </div>
---
+++ resources/views/blogs/show.blade.php
@@ -0,0 +1,17 @@
+@section('title', 'Blog ' . $blog->id)
+
+<x-layout>
+    <a href="/blogs"
+        class="w-max mt-8 mb-4 border rounded-lg px-4 py-1 border-green-500 transition-all duration-200 hover:bg-green-500 hover:text-black font-semibold flex items-center gap-1">
+        {{-- left arrow svg --}}
+        <svg xmlns="http://www.w3.org/2000/svg" class="size-[1em]" viewBox="0 0 24 24">
+            <path fill="currentColor" d="m10 18l-6-6l6-6l1.4 1.45L7.85 11H20v2H7.85l3.55 3.55z" />
+        </svg>
+        Back
+    </a>
+    <h2 class="text-left my-4 text-2xl font-bold">
+        <span class="text-white">(#{{ $blog->id }})</span>
+        <span class="text-green-500">{{ $blog->title }}</span>
+    </h2>
+    <p class="font-normal text-gray-400 text-lg">{{ $blog->body }}</p>
+</x-layout>
