267e01f5b5839d081e27020ba257a125ba751a7e
samithseu <samithseu2002@gmail.com>
2025-02-23 09:33:15 +0700

    changed little style

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <p class="text-center font-medium text-lg">Click the button belows to view all blogs!</p>
     <div class="flex items-center justify-center mt-4">
         <a href="/blogs"
-            class="bg-green-500 rounded-lg px-4 py-2 text-black font-bold transition duration-200 hover:bg-green-700">All
+            class="bg-green-500 rounded-lg px-4 py-1 text-black font-bold transition duration-200 hover:bg-green-700">All
             blogs
         </a>
     </div>
