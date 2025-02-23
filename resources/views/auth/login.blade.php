afcfbfdbb0c536474d91d7a57a5fe0bfeb45c702
samithseu <samithseu2002@gmail.com>
2025-02-23 09:34:16 +0700

    added 2 new files `login.blade.php` & `register.blade.php`

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,5 @@
+@section('title', 'Login')
+
+<x-layout>
+    <x-section-title>Login to your account!</x-section-title>
+</x-layout>
---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,6 @@
+@section('title', 'Register')
+
+<x-layout>
+    <x-section-title>Register New Account!</x-section-title>
+
+</x-layout>
