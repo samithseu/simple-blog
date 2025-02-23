1178114f38008adfda94432499ea20b52f84ffa2
samithseu <samithseu2002@gmail.com>
2025-02-23 09:33:56 +0700

    used AuthController in `web.php`

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,29 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BlogController;
 
+// root - home
 Route::get('/', function () {
     return view('welcome');
 })->name("home");
 
+// about
 Route::get('/about', function () {
     return view('about');
 })->name("about");
 
+// projects
 Route::get('/projects', function () {
     return view('projects');
 })->name("projects");
 
+// auth
+Route::get('/register', [AuthController::class, "showRegister"])->name("auth.show.register");
+Route::get('/login', [AuthController::class, "showLogin"])->name("auth.show.login");
+
+// blogs
 Route::get('/blogs', [BlogController::class, "index"])->name("blogs");
 Route::get('/blogs/create', [BlogController::class, "create"])->name("blog.create");
 Route::get('/blogs/{blog}', [BlogController::class, "show"])->name("blog.show");
