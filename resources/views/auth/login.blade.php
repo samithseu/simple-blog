307f079ba9250f5339075056095d044ddaa1acaa
samithseu <samithseu2002@gmail.com>
2025-02-23 09:50:37 +0700

    added `login` & `register` pages

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,4 +2,12 @@
 
 <x-layout>
     <x-section-title>Login to your account!</x-section-title>
+    <form action="{{ route('auth.login') }}" method="POST" class="w-full grid grid-cols-1 gap-6">
+        @csrf
+        <input class="px-4 py-2 rounded-lg text-lg" type="email" name="email" id="email" required
+            placeholder="Enter email..." value="{{ old('email') }}">
+        <input class="px-4 py-2 rounded-lg text-lg" type="password" name="password" id="password" required
+            placeholder="Enter password...">
+        <button class="w-full py-2 rounded-lg bg-green-500/30 hover:bg-green-500 text-lg" type="submit">Login</button>
+    </form>
 </x-layout>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,5 +2,13 @@
 
 <x-layout>
     <x-section-title>Register New Account!</x-section-title>
-
+    <form action="{{ route('auth.register') }}" method="POST" class="w-full grid grid-cols-1 gap-6">
+        @csrf
+        <input class="px-4 py-2 rounded-lg text-lg" type="email" name="email" id="email" required
+            placeholder="Enter email..." value="{{ old('email') }}">
+        <input class="px-4 py-2 rounded-lg text-lg" type="password" name="password" id="password" required
+            placeholder="Enter password...">
+        <button class="w-full py-2 rounded-lg bg-green-500/30 hover:bg-green-500 text-lg"
+            type="submit">Register</button>
+    </form>
 </x-layout>
