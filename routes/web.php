da11c41a3105ffe2d555aaf7fe539274b82d8a76
samithseu <samithseu2002@gmail.com>
2025-02-23 09:50:24 +0700

    used new `POST` functions

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,9 @@ Route::get('/projects', function () {
 
 // auth
 Route::get('/register', [AuthController::class, "showRegister"])->name("auth.show.register");
+Route::post('/register', [AuthController::class, "register"])->name("auth.register");
 Route::get('/login', [AuthController::class, "showLogin"])->name("auth.show.login");
+Route::post('/login', [AuthController::class, "login"])->name("auth.login");
 
 // blogs
 Route::get('/blogs', [BlogController::class, "index"])->name("blogs");
