112ac61c8227936463d1a90415ae3ef664319a1a
samithseu <samithseu2002@gmail.com>
2025-02-23 09:11:43 +0700

    changed border styles for each blog

--- resources/views/blogs/index.blade.php
+++ resources/views/blogs/index.blade.php
@@ -5,7 +5,7 @@
     <div class="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mb-4">
         @foreach ($blogs as $blog)
             <div
-                class="flex flex-col gap-2 border rounded-xl border-green-500 p-4 bg-gradient-to-br hover:from-green-500/40 hover:to-transparent transition-all duration-300">
+                class="flex flex-col gap-2 border rounded-xl border-green-500/30 p-4 bg-gradient-to-br hover:from-green-500/30 hover:to-transparent transition-all duration-300">
                 <a href="{{ route('blog.show', $blog->id) }}"
                     class="text-xl font-bold line-clamp-1 mb-2">{{ $blog->title }}</a>
                 <p class="text-sm text-gray-500 line-clamp-2">{{ $blog->body }}</p>
