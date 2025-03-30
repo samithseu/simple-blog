d1d1e8f3830cab65143f10f17ec1f673ee58e144
samithseu <samithseu2002@gmail.com>
2025-03-31 00:52:36 +0700

    added `sample-og-img.png` for `og:img` and other SEO tags

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,6 +5,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta property="og:title" content="Jroch App - Simple Blogs App" />
+    <meta name="description" content="A little Laravel application created for storing blogs." />
+    <meta property="og:description" content="A little Laravel application created for storing blogs." />
+    <meta property="og:image" content="{{ public_path('sample-og-img.png') }}" />
+
     <title>
         @hasSection('title')
             Jroch App - @yield('title')
