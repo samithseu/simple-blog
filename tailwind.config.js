8b7fb1323eefa841e26d2264d91b6d44cd033eed
samithseu <samithseu2002@gmail.com>
2025-03-31 00:22:45 +0700

    added `show-message` animation for showing session messages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,34 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
+import defaultTheme from "tailwindcss/defaultTheme";
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/**/*.blade.php',
-        './resources/**/*.js',
-        './resources/**/*.vue',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
     ],
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ["Figtree", ...defaultTheme.fontFamily.sans],
+            },
+            keyframes: {
+                "show-message": {
+                    "0%,100%": {
+                        translate: "-50% -150%",
+                    },
+                    "15%": {
+                        translate: "-50% 0",
+                    },
+                    "85%": {
+                        translate: "-50% 0",
+                    },
+                },
+            },
+            animation: {
+                message: "show-message 3.5s ease-out",
             },
         },
     },
