22ee54089aa17fdaf6623659966c1c40fd23a786
samithseu <samithseu2002@gmail.com>
2025-03-31 00:52:52 +0700

    added little border at each input box

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,14 +6,15 @@
 
     <form action="{{ route('auth.register') }}" method="POST" class="w-full grid grid-cols-1 gap-6">
         @csrf
-        <input class="px-4 py-2 rounded-lg text-lg" type="text" name="name" id="name" required
-            placeholder="Enter name..." value="{{ old('name') }}">
-        <input class="px-4 py-2 rounded-lg text-lg" type="email" name="email" id="email" required
-            placeholder="Enter email..." value="{{ old('email') }}">
-        <input class="px-4 py-2 rounded-lg text-lg" type="password" name="password" id="password" required
-            placeholder="Enter password...">
-        <input class="px-4 py-2 rounded-lg text-lg" type="password" name="password_confirmation"
-            id="password_confirmation" required placeholder="Enter password confirmation...">
+        <input class="border border-green-500/20 px-4 py-2 rounded-lg text-lg" type="text" name="name" id="name"
+            required placeholder="Enter name..." value="{{ old('name') }}">
+        <input class="border border-green-500/20 px-4 py-2 rounded-lg text-lg" type="email" name="email"
+            id="email" required placeholder="Enter email..." value="{{ old('email') }}">
+        <input class="border border-green-500/20 px-4 py-2 rounded-lg text-lg" type="password" name="password"
+            id="password" required placeholder="Enter password...">
+        <input class="border border-green-500/20 px-4 py-2 rounded-lg text-lg" type="password"
+            name="password_confirmation" id="password_confirmation" required
+            placeholder="Enter password confirmation...">
         <button class="w-full py-2 rounded-lg bg-green-500/30 hover:bg-green-500 text-lg"
             type="submit">Register</button>
     </form>
--- resources/views/blogs/create.blade.php
+++ resources/views/blogs/create.blade.php
@@ -6,10 +6,10 @@
 
     <form action="{{ route('blog.store') }}" method="POST" class="w-full grid grid-cols-1 gap-6">
         @csrf
-        <input class="px-4 py-2 rounded-lg text-lg" type="text" name="title" id="title" required maxlength="75"
-            placeholder="Enter title for your blog..." value="{{ old('title') }}">
-        <textarea class="px-4 py-2 rounded-lg text-lg resize-y" rows="6" name="body" id="body"
-            placeholder="Enter description for your blog..." required value="{{ old('body') }}"></textarea>
+        <input class="border border-green-500/20 px-4 py-2 rounded-lg text-lg" type="text" name="title" id="title"
+            required maxlength="75" placeholder="Enter title for your blog..." value="{{ old('title') }}">
+        <textarea class="border border-green-500/20 px-4 py-2 rounded-lg text-lg resize-y" rows="6" name="body"
+            id="body" placeholder="Enter description for your blog..." required value="{{ old('body') }}"></textarea>
         <button class="w-full py-2 rounded-lg bg-green-500/30 hover:bg-green-500 text-lg" type="submit">Create
             Now!</button>
     </form>
