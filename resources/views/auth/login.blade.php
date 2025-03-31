50691d5b0d7360f95d43269ec5ae767275a1019c
samithseu <samithseu2002@gmail.com>
2025-03-31 13:02:01 +0700

    added options to switch to each other in `sign-in` and `register`

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,12 +4,16 @@
     <x-section-title>Login to your account!</x-section-title>
     <x-error :error="$errors" />
 
-    <form action="{{ route('auth.login') }}" method="POST" class="w-full grid grid-cols-1 gap-6">
+    <form action="{{ route('auth.login') }}" method="POST" class="w-full grid grid-cols-1 lg:grid-cols-2 gap-6">
         @csrf
         <input class="border border-green-500/20 px-4 py-2 rounded-lg text-lg" type="email" name="email" id="email"
             required placeholder="Enter email..." value="{{ old('email') }}">
         <input class="border border-green-500/20 px-4 py-2 rounded-lg text-lg" type="password" name="password"
             id="password" required placeholder="Enter password...">
-        <button class="w-full py-2 rounded-lg bg-green-500/30 hover:bg-green-500 text-lg" type="submit">Login</button>
+        <button
+            class="w-full lg:col-span-2  py-2 rounded-lg bg-green-500/30 hover:bg-green-500 text-lg transition-all duration-200"
+            type="submit">Login</button>
     </form>
+    <p class="text-center text-sm mt-3 text-zinc-300">Don't have an account? <a class=" text-green-500 font-bold"
+            href="{{ route('auth.register') }}">Register</a></p>
 </x-layout>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,7 +4,7 @@
     <x-section-title>Register New Account!</x-section-title>
     <x-error :error="$errors" />
 
-    <form action="{{ route('auth.register') }}" method="POST" class="w-full grid grid-cols-1 gap-6">
+    <form action="{{ route('auth.register') }}" method="POST" class="w-full grid grid-cols-1 lg:grid-cols-2 gap-6">
         @csrf
         <input class="border border-green-500/20 px-4 py-2 rounded-lg text-lg" type="text" name="name" id="name"
             required placeholder="Enter name..." value="{{ old('name') }}">
@@ -15,7 +15,10 @@
         <input class="border border-green-500/20 px-4 py-2 rounded-lg text-lg" type="password"
             name="password_confirmation" id="password_confirmation" required
             placeholder="Enter password confirmation...">
-        <button class="w-full py-2 rounded-lg bg-green-500/30 hover:bg-green-500 text-lg"
+        <button
+            class="w-full lg:col-span-2 py-2 rounded-lg bg-green-500/30 hover:bg-green-500 text-lg transition-all duration-200"
             type="submit">Register</button>
     </form>
+    <p class="text-center text-sm mt-3 text-zinc-300">Already have an account? <a class="text-green-500 font-bold"
+            href="{{ route('auth.show.login') }}">Login</a></p>
 </x-layout>
